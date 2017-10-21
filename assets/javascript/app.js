526ae498697db2ef79baa6683356d6d38a8c2d85
Fernando Fuentes <fernando.fuentes1113@gmail.com>
2017-10-21 13:14:47 -0500

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,27 @@
-var searchTerm = "trump";
+$(document).ready(function () {
 
-var url = "https://api.nytimes.com/svc/archive/v1/2016/1.json" + searchTerm + "&api-key=bf54a6307b9545ac8f98e8edddec3c3e";
+    var searchTerm = "trump";
+
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'api-key': "bf54a6307b9545ac8f98e8edddec3c3e"
+    });
+
+    console.log(url);
 
 
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
+    $.ajax({
+        url: url,
+        method: 'GET'
+    }).done(function (result) {
+        console.log(result);
+    }).fail(function (err) {
+        throw err;
+    });
+
+
+    // Display the "TITLE" of the article
+    $("#title").text("")
+
+
 });
